47151e9c65a40db92ed7526c764b70b69ddcdb2a
Michael M <michaelarceus96@gmail.com>
2019-10-09 04:28:17 +0700

    Updates

---
+++ precache-manifest.f0f89291804c9905d3db8891ee9fa118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3189dd15fe25ba927182bf782842058",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5069f5ef4a5ebaa95d9f",
+    "url": "/monsters-rolodex/static/css/main.0955c61c.chunk.css"
+  },
+  {
+    "revision": "d077ac5370be5be9e4d1",
+    "url": "/monsters-rolodex/static/js/2.868343f9.chunk.js"
+  },
+  {
+    "revision": "5069f5ef4a5ebaa95d9f",
+    "url": "/monsters-rolodex/static/js/main.eb51e47b.chunk.js"
+  },
+  {
+    "revision": "42280f1e7ca649a3b03a",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1619bbd.js"
+  }
+]);
